ba407c5b4c3e3656ee251362d080ed3306ab8d99
Awia001 <awiamartindale@googlemail.com>
2019-08-29 19:34:40 +0100

    Added more cmake stuff (still doesn't work)

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -106,12 +106,9 @@ void terminal_writestring(const char* data)
 }
  
 #if defined(__cplusplus)
-extern "C" /* Use C linkage for kernel_main. */
 #endif
 void kernel_main()
 {
 	terminal_initialize();
-	/* Since there is no support for newlines in terminal_putchar yet, \n will
-	   produce some VGA specific character instead. This is normal. */
 	terminal_writestring("Hello, kernel World!\n");
 }
